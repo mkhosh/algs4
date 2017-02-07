755bebd5daaf5fd4289513c8a57d43a158402568
Moe Khoshneshin <mkhoshneshin@apple.com>
2017-02-06 22:03:25 -0800

    added git ignore

--- src/main/java/edu/princeton/cs/algs4/WeightedQuickUnionUF.java
+++ src/main/java/edu/princeton/cs/algs4/WeightedQuickUnionUF.java
@@ -13,71 +13,71 @@
 package edu.princeton.cs.algs4;
 
 /**
- *  The {@code WeightedQuickUnionUF} class represents a <em>union–find data type</em>
- *  (also known as the <em>disjoint-sets data type</em>).
- *  It supports the <em>union</em> and <em>find</em> operations,
- *  along with a <em>connected</em> operation for determining whether
- *  two sites are in the same component and a <em>count</em> operation that
- *  returns the total number of components.
- *  <p>
- *  The union–find data type models connectivity among a set of <em>n</em>
- *  sites, named 0 through <em>n</em>–1.
- *  The <em>is-connected-to</em> relation must be an 
- *  <em>equivalence relation</em>:
- *  <ul>
- *  <li> <em>Reflexive</em>: <em>p</em> is connected to <em>p</em>.
- *  <li> <em>Symmetric</em>: If <em>p</em> is connected to <em>q</em>,
- *       then <em>q</em> is connected to <em>p</em>.
- *  <li> <em>Transitive</em>: If <em>p</em> is connected to <em>q</em>
- *       and <em>q</em> is connected to <em>r</em>, then
- *       <em>p</em> is connected to <em>r</em>.
- *  </ul>
- *  <p>
- *  An equivalence relation partitions the sites into
- *  <em>equivalence classes</em> (or <em>components</em>). In this case,
- *  two sites are in the same component if and only if they are connected.
- *  Both sites and components are identified with integers between 0 and
- *  <em>n</em>–1. 
- *  Initially, there are <em>n</em> components, with each site in its
- *  own component.  The <em>component identifier</em> of a component
- *  (also known as the <em>root</em>, <em>canonical element</em>, <em>leader</em>,
- *  or <em>set representative</em>) is one of the sites in the component:
- *  two sites have the same component identifier if and only if they are
- *  in the same component.
- *  <ul>
- *  <li><em>union</em>(<em>p</em>, <em>q</em>) adds a
- *      connection between the two sites <em>p</em> and <em>q</em>.
- *      If <em>p</em> and <em>q</em> are in different components,
- *      then it replaces
- *      these two components with a new component that is the union of
- *      the two.
- *  <li><em>find</em>(<em>p</em>) returns the component
- *      identifier of the component containing <em>p</em>.
- *  <li><em>connected</em>(<em>p</em>, <em>q</em>)
- *      returns true if both <em>p</em> and <em>q</em>
- *      are in the same component, and false otherwise.
- *  <li><em>count</em>() returns the number of components.
- *  </ul>
- *  <p>
- *  The component identifier of a component can change
- *  only when the component itself changes during a call to
- *  <em>union</em>—it cannot change during a call
- *  to <em>find</em>, <em>connected</em>, or <em>count</em>.
- *  <p>
- *  This implementation uses weighted quick union by size (without path compression).
- *  Initializing a data structure with <em>n</em> sites takes linear time.
- *  Afterwards, the <em>union</em>, <em>find</em>, and <em>connected</em>
- *  operations  take logarithmic time (in the worst case) and the
- *  <em>count</em> operation takes constant time.
- *  For alternate implementations of the same API, see
- *  {@link UF}, {@link QuickFindUF}, and {@link QuickUnionUF}.
+ * The {@code WeightedQuickUnionUF} class represents a <em>union–find data type</em>
+ * (also known as the <em>disjoint-sets data type</em>).
+ * It supports the <em>union</em> and <em>find</em> operations,
+ * along with a <em>connected</em> operation for determining whether
+ * two sites are in the same component and a <em>count</em> operation that
+ * returns the total number of components.
+ * <p>
+ * The union–find data type models connectivity among a set of <em>n</em>
+ * sites, named 0 through <em>n</em>–1.
+ * The <em>is-connected-to</em> relation must be an
+ * <em>equivalence relation</em>:
+ * <ul>
+ * <li> <em>Reflexive</em>: <em>p</em> is connected to <em>p</em>.
+ * <li> <em>Symmetric</em>: If <em>p</em> is connected to <em>q</em>,
+ * then <em>q</em> is connected to <em>p</em>.
+ * <li> <em>Transitive</em>: If <em>p</em> is connected to <em>q</em>
+ * and <em>q</em> is connected to <em>r</em>, then
+ * <em>p</em> is connected to <em>r</em>.
+ * </ul>
+ * <p>
+ * An equivalence relation partitions the sites into
+ * <em>equivalence classes</em> (or <em>components</em>). In this case,
+ * two sites are in the same component if and only if they are connected.
+ * Both sites and components are identified with integers between 0 and
+ * <em>n</em>–1.
+ * Initially, there are <em>n</em> components, with each site in its
+ * own component.  The <em>component identifier</em> of a component
+ * (also known as the <em>root</em>, <em>canonical element</em>, <em>leader</em>,
+ * or <em>set representative</em>) is one of the sites in the component:
+ * two sites have the same component identifier if and only if they are
+ * in the same component.
+ * <ul>
+ * <li><em>union</em>(<em>p</em>, <em>q</em>) adds a
+ * connection between the two sites <em>p</em> and <em>q</em>.
+ * If <em>p</em> and <em>q</em> are in different components,
+ * then it replaces
+ * these two components with a new component that is the union of
+ * the two.
+ * <li><em>find</em>(<em>p</em>) returns the component
+ * identifier of the component containing <em>p</em>.
+ * <li><em>connected</em>(<em>p</em>, <em>q</em>)
+ * returns true if both <em>p</em> and <em>q</em>
+ * are in the same component, and false otherwise.
+ * <li><em>count</em>() returns the number of components.
+ * </ul>
+ * <p>
+ * The component identifier of a component can change
+ * only when the component itself changes during a call to
+ * <em>union</em>—it cannot change during a call
+ * to <em>find</em>, <em>connected</em>, or <em>count</em>.
+ * <p>
+ * This implementation uses weighted quick union by size (without path compression).
+ * Initializing a data structure with <em>n</em> sites takes linear time.
+ * Afterwards, the <em>union</em>, <em>find</em>, and <em>connected</em>
+ * operations  take logarithmic time (in the worst case) and the
+ * <em>count</em> operation takes constant time.
+ * For alternate implementations of the same API, see
+ * {@link UF}, {@link QuickFindUF}, and {@link QuickUnionUF}.
+ * <p>
+ * <p>
+ * For additional documentation, see <a href="http://algs4.cs.princeton.edu/15uf">Section 1.5</a> of
+ * <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
  *
- *  <p>
- *  For additional documentation, see <a href="http://algs4.cs.princeton.edu/15uf">Section 1.5</a> of
- *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
- *
- *  @author Robert Sedgewick
- *  @author Kevin Wayne
+ * @author Robert Sedgewick
+ * @author Kevin Wayne
  */
 public class WeightedQuickUnionUF {
     private int[] parent;   // parent[i] = parent of i
@@ -86,10 +86,10 @@ public class WeightedQuickUnionUF {
 
     /**
      * Initializes an empty union–find data structure with {@code n} sites
-     * {@code 0} through {@code n-1}. Each site is initially in its own 
+     * {@code 0} through {@code n-1}. Each site is initially in its own
      * component.
      *
-     * @param  n the number of sites
+     * @param n the number of sites
      * @throws IllegalArgumentException if {@code n < 0}
      */
     public WeightedQuickUnionUF(int n) {
@@ -110,11 +110,11 @@ public class WeightedQuickUnionUF {
     public int count() {
         return count;
     }
-  
+
     /**
      * Returns the component identifier for the component containing site {@code p}.
      *
-     * @param  p the integer representing one object
+     * @param p the integer representing one object
      * @return the component identifier for the component containing site {@code p}
      * @throws IndexOutOfBoundsException unless {@code 0 <= p < n}
      */
@@ -129,32 +129,32 @@ public class WeightedQuickUnionUF {
     private void validate(int p) {
         int n = parent.length;
         if (p < 0 || p >= n) {
-            throw new IndexOutOfBoundsException("index " + p + " is not between 0 and " + (n-1));  
+            throw new IndexOutOfBoundsException("index " + p + " is not between 0 and " + (n - 1));
         }
     }
 
     /**
      * Returns true if the the two sites are in the same component.
      *
-     * @param  p the integer representing one site
-     * @param  q the integer representing the other site
+     * @param p the integer representing one site
+     * @param q the integer representing the other site
      * @return {@code true} if the two sites {@code p} and {@code q} are in the same component;
-     *         {@code false} otherwise
+     * {@code false} otherwise
      * @throws IndexOutOfBoundsException unless
-     *         both {@code 0 <= p < n} and {@code 0 <= q < n}
+     *                                   both {@code 0 <= p < n} and {@code 0 <= q < n}
      */
     public boolean connected(int p, int q) {
         return find(p) == find(q);
     }
 
     /**
-     * Merges the component containing site {@code p} with the 
+     * Merges the component containing site {@code p} with the
      * the component containing site {@code q}.
      *
-     * @param  p the integer representing one site
-     * @param  q the integer representing the other site
+     * @param p the integer representing one site
+     * @param q the integer representing the other site
      * @throws IndexOutOfBoundsException unless
-     *         both {@code 0 <= p < n} and {@code 0 <= q < n}
+     *                                   both {@code 0 <= p < n} and {@code 0 <= q < n}
      */
     public void union(int p, int q) {
         int rootP = find(p);
@@ -165,8 +165,7 @@ public class WeightedQuickUnionUF {
         if (size[rootP] < size[rootQ]) {
             parent[rootP] = rootQ;
             size[rootQ] += size[rootP];
-        }
-        else {
+        } else {
             parent[rootQ] = rootP;
             size[rootP] += size[rootQ];
         }
@@ -175,7 +174,7 @@ public class WeightedQuickUnionUF {
 
 
     /**
-     * Reads in a sequence of pairs of integers (between 0 and n-1) from standard input, 
+     * Reads in a sequence of pairs of integers (between 0 and n-1) from standard input,
      * where each integer represents some object;
      * if the sites are in different components, merge the two components
      * and print the pair to standard output.
