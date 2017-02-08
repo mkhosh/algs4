deabffe7ed8d04645eaef56375bac3139623ba46
Moe Khoshneshin <mkhoshneshin@apple.com>
2017-02-07 22:22:33 -0800

    finished Deque implementation

--- src/main/java/hw2/Deque.java
+++ src/main/java/hw2/Deque.java
@@ -1,6 +1,7 @@
 package hw2;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /**
  * Created by mkhoshneshin on 2/7/17.
@@ -15,7 +16,7 @@ public class Deque<Item> implements Iterable<Item> {
         private Node next;
         private Node prev;
 
-        private Node(Item it, Node ne, Node pr) {
+        private Node(Item it, Node pr, Node ne) {
             item = it;
             next = ne;
             prev = pr;
@@ -32,7 +33,7 @@ public class Deque<Item> implements Iterable<Item> {
 
     // is the deque empty?
     public boolean isEmpty() {
-        return first == null;
+        return first == null || last == null;
     }
 
     // return the number of items on the deque
@@ -42,36 +43,76 @@ public class Deque<Item> implements Iterable<Item> {
 
     // add the item to the front
     public void addFirst(Item item) {
-
+        if (item==null) throw new NullPointerException("Item is null");
+        first = new Node(item, null, first);
+        if (last == null) last = first;
+        else first.next.prev = first;
+        n++;
+        assert check();
     }
 
     // add the item to the end
     public void addLast(Item item) {
-
+        if (item==null) throw new NullPointerException("Item is null");
+        last = new Node(item, last, null);
+        if (first == null) first = last;
+        else last.prev.next = last;
+        n++;
+        assert check();
     }
 
     // remove and return the item from the front
     public Item removeFirst() {
-
+        if (isEmpty()) throw new NoSuchElementException("Deque underflow");
+        Item item = first.item;
+        first = first.next;
+        if (isEmpty()) last = null;
+        else first.prev = null;
+        n--;
+        assert check();
+        return item;
     }
 
     // remove and return the item from the end
     public Item removeLast() {
-
+        if (isEmpty()) throw new NoSuchElementException("Deque underflow");
+        Item item = last.item;
+        last = last.prev;
+        if (isEmpty()) first = null;
+        else last.next = null;
+        n--;
+        assert check();
+        return item;
     }
 
     // return an iterator over items in order from front to end
     public Iterator<Item> iterator() {
-
+        return new DequeIterator();
     }
 
-    // unit testing (optional)
-    public static void main(String[] args) {
+    private class DequeIterator implements Iterator<Item> {
+        private Node current = first;
 
+
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+
+        public Item next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
     }
 
     // check internal invariants
     private boolean check() {
+        if (first.prev != null || last.next != null) return false;
         if (n < 0) {
             return false;
         } else if (n == 0) {
@@ -81,6 +122,7 @@ public class Deque<Item> implements Iterable<Item> {
             if (first == null || last == null) return false;
             if (first != last) return false;
             if (first.next != null) return false;
+            if (last.prev != null) return false;
         } else {
             if (first == null || last == null) return false;
             if (first == last) return false;
@@ -89,7 +131,13 @@ public class Deque<Item> implements Iterable<Item> {
 
             // check internal consistency of instance variable n
             int numberOfNodes = 0;
-            for (Node x = first; x != null && numberOfNodes <= n; x = x.next) {
+            for (Node x = first; x != null; x = x.next) {
+                numberOfNodes++;
+            }
+            if (numberOfNodes != n) return false;
+
+            numberOfNodes = 0;
+            for (Node x = last; x != null; x = x.prev) {
                 numberOfNodes++;
             }
             if (numberOfNodes != n) return false;
@@ -100,8 +148,18 @@ public class Deque<Item> implements Iterable<Item> {
                 lastNode = lastNode.next;
             }
             if (last != lastNode) return false;
+
+            Node firstNode = last;
+            while (firstNode.prev != null) {
+                firstNode = firstNode.prev;
+            }
+            if (first != firstNode) return false;
         }
 
         return true;
+    }
+
+    // unit testing (optional)
+    public static void main(String[] args) {
     }
 }
